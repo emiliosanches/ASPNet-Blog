0bbd30ab487ceea9930f9da82c8a511ac7db004e
Emilio <emiliosneto13@gmail.com>
2023-03-24 12:29:17 -0300

    Initial commit
    Start ASP.Net MVC project

---
+++ ASPNet Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNet_Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
